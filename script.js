3121fb58803f34b6e39c7ca0fe375d9df4467b1e
Ali Aliev <ali.aliev.msk>
2023-02-09 23:12:57 +0300

    initial

---
+++ script.js
@@ -0,0 +1,32 @@
+let li = document.getElementsByTagName("li")
+let ul = document.getElementsByTagName("ul")
+
+function refresh(){
+    for (let i = 0; i < li.length; i++) {
+        if (randomInt() % 4 === 0) {
+            li[i].classList.toggle("hide_font")
+        }
+    }
+
+    for (let i = 0; i < ul.length; i++) {
+        if (randomInt() % 34 === 0) {
+            ul[i].classList.toggle("hide_font")
+        }
+    }
+
+}
+
+window.onload = refresh()
+
+
+
+
+function timedRefresh(timeoutPeriod) {
+        return setTimeout("location.reload();", timeoutPeriod);
+}
+
+window.onload = timedRefresh(50000);
+
+function randomInt(){
+    return Math.floor(Math.random() * 1146)
+}
