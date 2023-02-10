e8f1603a8fe4bec976b77681a05b2533f17db1dd
Ali Aliev <ali.aliev.msk>
2023-02-10 21:38:17 +0300

    F-key add

--- script.js
+++ script.js
@@ -4,12 +4,18 @@ let ul = document.getElementsByTagName("ul")
 function refresh(){
     for (let i = 0; i < li.length; i++) {
         if (randomInt() % 4 === 0) {
+            if (!li[i].classList.contains("hide_font")){
             li[i].classList.toggle("hide_font")
-        }
+        } else {
+                if (randomInt() % 34 === 0){
+                    li[i].classList.remove("hide_font")
+                }
+            }
     }
+}
 
     for (let i = 0; i < ul.length; i++) {
-        if (randomInt() % 34 === 0) {
+        if (randomInt() % 343 === 0) {
             ul[i].classList.toggle("hide_font")
         }
     }
@@ -23,7 +29,7 @@ function timedRefresh(timeoutPeriod) {
         return setTimeout("location.reload();", timeoutPeriod);
 }
 
-window.onload = timedRefresh(50000);
+window.onload = timedRefresh(500000);
 
 function randomInt(){
     return Math.floor(Math.random() * 1146)
